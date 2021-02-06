fd1e61dce3e000ecf296dab013a9910e203ff608
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-02-06 22:27:59 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Button,
+  CardTitle,
+  CardText,
+} from "reactstrap";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
+  const [randomJoke, setRandomJoke] = useState([]);
+
+  const url = "https://official-joke-api.appspot.com/jokes/programming/random";
+
+  const fetchJokes = async () => {
+    const res = await fetch(url);
+    const joke = await res.json();
+    setRandomJoke(joke);
+  };
+
+  useEffect(() => {
+    fetchJokes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar className="navbar navbar-dark bg-dark">
+        <NavbarBrand href="/" className="m-auto p-1">
+          Random Programming Jokes
+        </NavbarBrand>
+      </Navbar>
+      <main>
+        <div className="container">
+          {randomJoke.map((joke) => {
+            const { id, setup, punchline } = joke;
+            return (
+              <Card
+                body
+                className="text-center card"
+                style={{ width: "18rem" }}
+                key={id}
+              >
+                <CardBody className="m-auto p-2">
+                  <CardTitle tag="h5">{setup}</CardTitle>
+                  <CardText>{punchline}</CardText>
+                  <Button onClick={fetchJokes} className="btn btn-dark">
+                    Generate Joke
+                  </Button>
+                </CardBody>
+              </Card>
+            );
+          })}
+        </div>
+      </main>
+    </>
   );
 }
 
